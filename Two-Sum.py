0bc1bd939be2019d3e92e3747452194ca7d2f864
Markus  Rabin R <115344060+haiser1@users.noreply.github.com>
2025-06-28 15:46:43 +0700

    [1] [Time Beats: 100.00%] [Memory Beats: 10.69%] - LeetPush

---
+++ Two-Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in num_map:
+                return [num_map[comp], i]
+            num_map[num] = i
+
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return[i,j]
+
+
+            
+
+            
+        
